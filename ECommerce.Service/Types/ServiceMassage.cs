b0658c361251a0fb2b2109a648a215e1d7a37dfc
enes1094 <enes.tikici123@hotmail.com>
2025-10-07 01:35:01 +0300

    Add Types folder and data protector

---
+++ ECommerce.Service/Types/ServiceMassage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Service.Types
+{
+    public class ServiceMassage
+    {
+        public bool IsSucced { get; set; }
+        public string Massage { get; set; }
+
+    }
+
+    public class ServiceMassage<T> : ServiceMassage
+    {
+        public T? Data { get; set; }
+    }
+}
--- ECommerce.Web/Program.cs
+++ ECommerce.Web/Program.cs
@@ -1,6 +1,7 @@
 using ECommerce.Data.Context;
 using ECommerce.Data.Repository;
 using ECommerce.Data.UnitOfWork;
+using ECommerce.Service.DataProtector;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,11 +9,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var keysDirectory = new DirectoryInfo(Path.Combine(builder.Environment.ContentRootPath, "App_Data", "Keys"));
+
+// DataProtection servisi ekle
+builder.Services.AddDataProtection()
+    .SetApplicationName("ECommerce")
+    .PersistKeysToFileSystem(keysDirectory);
+
 builder.Services.AddDbContext<ECommerceDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IUnitOfWork, IUnitOfWork>();
+builder.Services.AddScoped<IDataProtector, DataProtector>();
 
 var app = builder.Build();
 
